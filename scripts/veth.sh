2e279d71e15eb1a4e44af6cbdbeb89042864afd4
Ângelo Lapolli <aclapolli@inf.ufrgs.br>
2020-12-25 17:59:50 -0300

    Add license notice to source files

--- scripts/veth.sh
+++ scripts/veth.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# "Offloading Real-time DDoS Attack Detection to Programmable Data Planes" (IM 2019)
+# Copyright (C) 2019  Ângelo Lapolli
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 if [ $(id -u) != "0" ]
 then
